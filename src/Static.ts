ac215d1d58c11f6b9949b1c396b49c46e50abacd
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-05-27 19:31:45 +0200

    add style as a separte css file

--- src/Static.ts
+++ src/Static.ts
@@ -16,19 +16,7 @@ class Static {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Image Processing</title>
-                <style>
-                    body{font-size:1.25em; line-height:1.5;}
-                    .navigation{display:flex; align-items:center; justify-content:center;}
-                    .navigation a{padding:10px 20px; text-decoration:none; font-size:24px; color:#555;}
-                    .gallery{display:flex; align-items:center; justify-content:center; flex-wrap:wrap;}
-                    .gallery .thumb-image{width:200px; height:auto;}
-                    .gallery figcaption{text-align:center;}
-                    .gallery figure{margin:15px; padding:15px; box-shadow:0px 2px 10px 0 rgba(0,0,0,0.1); transition:all 0.3s ease-in-out;}
-                    .gallery figure:hover{box-shadow:0px 5px 15px 0 rgba(0,0,0,0.1); transform:translateY(-5px);}
-                    .form-wrapper{display:flex; align-items:center; justify-content:center; margin:20px;}
-                    .form-wrapper form{display:inline-flex; align-items:center; justify-content:center; padding:20px; background-color:rgba(128,128,128,.3); border-radius:10px;}
-                    .form-wrapper form input, .form-wrapper form select{line-height:1.5; height:30px; margin:0 5px; padding:5px 10px; box-sizing:content-box;}
-                </style>
+                <link rel="stylesheet" id="style-css" href="/css/style.css" type="text/css" media="all">
             </head>
             <body>
                 <header class="navigation">
